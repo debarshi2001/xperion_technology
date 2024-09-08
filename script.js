77c74a066d9d57fe7f82ec76f224283bfca8f349
Debarshi Halder <debarshihalder89@gmail.com>
2024-09-08 20:55:17 +0530

    Second Commit

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ CardItems.forEach(element => {
 
 })
 console.log(CardItems)
+
+
+function checkUserAgent() {
+
+    var userAgent = navigator.userAgent.toLowerCase();
+  
+    if (userAgent.indexOf("android") > -1 || userAgent.indexOf("iphone") > -1) {
+  
+      // Redirect to mobile page or display a message
+  
+      alert("This website is optimized for desktop viewing. Please access on a computer."); 
+  
+    }
+  
+  }
+  
+  
+  
+  // Call the function on page load
+  
+  window.onload = checkUserAgent;
+  
